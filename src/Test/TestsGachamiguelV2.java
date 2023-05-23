1eafe30d567bf5fe737dbe3603b4fa85f5dc5e5a
Pedro <pedfercal2@alu.edu.gva.es>
2023-05-23 10:21:34 +0200

    Commit de la versión V2, con test depurados.

--- src/Test/TestsGachamiguelV2.java
+++ src/Test/TestsGachamiguelV2.java
@@ -4,83 +4,23 @@ import recetas.GachamiguelV2;
 
 public class TestsGachamiguelV2 {
     @Test
-    public void Test1(){
+    public void Test1V2(){
         String r = GachamiguelV2.cata(1);
         Assertions.assertEquals("1", r);
     }
     @Test
-    public void Test2(){
-        String r = GachamiguelV2.cata(2);
-        Assertions.assertEquals("2", r);
-    }
-    @Test
-    public void Test3(){
+    public void Test2V2(){
         String r = GachamiguelV2.cata(3);
         Assertions.assertEquals("Gacha", r);
     }
     @Test
-    public void Test4(){
-        String r = GachamiguelV2.cata(4);
-        Assertions.assertEquals("4", r);
-    }
-    @Test
-    public void Test5(){
+    public void Test3V2() {
         String r = GachamiguelV2.cata(5);
         Assertions.assertEquals("Miga", r);
     }
     @Test
-    public void Test6(){
-        String r = GachamiguelV2.cata(6);
-        Assertions.assertEquals("Gacha", r);
-    }
-    @Test
-    public void Test7(){
-        String r = GachamiguelV2.cata(7);
-        Assertions.assertEquals("7", r);
-    }
-    @Test
-    public void Test8(){
-        String r = GachamiguelV2.cata(8);
-        Assertions.assertEquals("8", r);
-    }
-    @Test
-    public void Test9(){
-        String r = GachamiguelV2.cata(9);
-        Assertions.assertEquals("Gacha", r);
-    }
-    @Test
-    public void Test10(){
-        String r = GachamiguelV2.cata(10);
-        Assertions.assertEquals("Miga", r);
-    }
-    @Test
-    public void Test11(){
-        String r = GachamiguelV2.cata(11);
-        Assertions.assertEquals("11", r);
-    }
-    @Test
-    public void Test12(){
-        String r = GachamiguelV2.cata(12);
-        Assertions.assertEquals("Gacha", r);
-    }
-    @Test
-    public void Test13(){
-        String r = GachamiguelV2.cata(13);
-        Assertions.assertEquals("13", r);
-    }
-    @Test
-    public void Test14(){
-        String r = GachamiguelV2.cata(14);
-        Assertions.assertEquals("14", r);
-    }
-    @Test
-    public void Test15(){
+    public void Test4V2(){
         String r = GachamiguelV2.cata(15);
-        Assertions.assertEquals("Gachamiga", r);
-    }
-    @Test
-    public void Test30(){
-        String r = GachamiguelV2.cata(30);
         Assertions.assertEquals("Gachamiga", r);
     }
 }
